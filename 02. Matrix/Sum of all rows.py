7f9af5a0c5305bcff9b78991130010158219dc36
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-02-28 18:52:56 +0530

    sum of all rows

--- 02. Matrix/Sum of all rows.py
+++ 02. Matrix/Sum of all rows.py
@@ -1,6 +1,15 @@
-def rowSum(mat):
+def rowSum1(mat):
     for i in range(len(mat)):
         print(sum(mat[i]))
 
+def rowSum(mat):
+    for i in range(len(mat)):
+        temp_sum = 0
+        for j in range(len(mat[0])):
+            temp_sum += mat[i][j]
+        print(temp_sum)
+        temp_sum = 0
+
+
 mat = [[1,2,3],[4,5,6],[7,8,9]]
-print(rowSum(mat))
+rowSum(mat)
