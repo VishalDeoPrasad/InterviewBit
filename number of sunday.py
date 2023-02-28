0fd9d68b07bb22f269268b6808366c3cb9926c6e
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-02-28 17:15:40 +0530

    number of sunday

---
+++ number of sunday.py
@@ -0,0 +1,9 @@
+class Solution:
+    # @param A : string
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        day_dic = {"Monday": 1, "Tuesday": 2, "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6, "Sunday": 7}
+        return (B+day_dic[A]-1)//7
+
+print(Solution().solve("Sunday", 1))
