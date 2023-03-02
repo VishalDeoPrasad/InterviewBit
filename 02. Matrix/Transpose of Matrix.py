2d0aa792f5f4e32610666120c87ee26d88d64cfb
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-03-02 15:30:08 +0530

    Transpose of matrix

--- 02. Matrix/Transpose of Matrix.py
+++ 02. Matrix/Transpose of Matrix.py
@@ -1,5 +1,14 @@
-def Transpose(M):
+def Transpose1(M):
     T = [[M[j][i] for j in range(len(M))] for i in range(len(M[0]))]
+    return T
+
+def Transpose(A):
+    T = []
+    for i in range(len(A[0])):
+        temp = []
+        for j in range(len(A)):
+            temp.append(A[j][i])
+        T.append(temp)
     return T
 
 mat = [[1,2,3],[4,5,6],[7,8,9]]
