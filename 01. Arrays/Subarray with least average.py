873a228663c38f0fa998196f4e4e747c740e149c
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-03-22 17:28:24 +0530

    subarray with least average

--- 01. Arrays/Subarray with least average.py
+++ 01. Arrays/Subarray with least average.py
@@ -1,15 +1,48 @@
 class Solution:
-    def solve(self, A, B):
-        least_avg = sum(A[:B])/B
-        idx = 0
+    def average(self,A,B,start,end):
+        sum = 0
+        for i in range(start, end):
+            sum += A[i]
+        return sum/B
+    
+    def solve1(self, A, B):
+        least_avg = 999999999
+        min_idx = 0
         for i in range(len(A)-B+1):
-            curr_avg = sum(A[i:i+B])/B
-            print(curr_avg)
-            if curr_avg < least_avg:
-                least_avg = curr_avg
-                idx = i
+            j = i+B
+            temp = self.average(A,B,i,j)
+            if least_avg > temp:
+                least_avg = temp
+                min_idx = i
+        return min_idx
+    
+    def solve(self, A, B):
+        least_sum = sum(A[:B])
+        axu = []
+        axu.append((least_sum, 0, B-1))
+        for i in range(B, len(A)):
+            least_sum = least_sum+A[i]-A[i-B]
+            axu.append((least_sum,i-B+1, i))
+        
+        avg = axu[0][0]/B
+        idx = axu[0][1]
+        for i in range(1, len(axu)):
+            if axu[i][0]/B < avg:
+                avg = axu[i][0]/B
+                idx = axu[i][1]
         return idx
 
-A = [ 20, 3, 13, 5, 10, 14, 8, 5, 11, 9, 1, 11 ]
-B = 9
-print(Solution().solve(A, B))
+        
+
+
+
+
+
+
+A1=[3, 7, 90, 20, 10, 50, 40]
+B1=3
+
+A2=[3, 7, 5, 20, -10, 0, 12]
+B2=2
+
+print(Solution().solve(A1, B1))
