d1f6f0374226075c300ea96e167cb050d0f71a52
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-05-02 22:44:54 +0530

    number of sunday

--- number of sunday.py
+++ number of sunday.py
@@ -6,4 +6,4 @@ class Solution:
         day_dic = {"Monday": 1, "Tuesday": 2, "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6, "Sunday": 7}
         return (B+day_dic[A]-1)//7
 
-print(Solution().solve("Sunday", 1))
+print(Solution().solve("Sunday", 1)) 
